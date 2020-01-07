273da4a29128346f04b2de85e0d024482ab24b8e
NaveedG <nganjani@protonmail.com>
2020-01-07 16:24:25 -0600

    node module reinstall

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
   <BrowserRouter>
     <div>
-      {/* <Route exact path='/' component={Home}/> */}
     </div>
   </BrowserRouter>
   );
