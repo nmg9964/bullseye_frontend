7a6eab2c1b56aa8acd742322a199962ccb030742
NaveedG <nganjani@protonmail.com>
2020-01-07 16:01:18 -0600

    react router install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
+  <BrowserRouter>
     <div>
+      {/* <Route exact path='/' component={Home}/> */}
     </div>
+  </BrowserRouter>
   );
 }
 
