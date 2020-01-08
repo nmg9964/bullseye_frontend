5ddf049eb65f1376121faaf12cf38ef079c8f268
NaveedG <nganjani@protonmail.com>
2020-01-08 11:45:17 -0600

    added home and login components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './Navbar.js'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
   <BrowserRouter>
     <div>
-      <Navbar></Navbar>
+      <Navbar />  
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/admin' component={Login}/>
     </div>
   </BrowserRouter>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,41 +5,41 @@ class Navbar extends React.Component {
   
   render() {
     return (
-      <div class='ui secondary menu'>
-        <img src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' class='ui big image'/>
+      <div className='ui secondary menu'>
+        <img src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' className='ui big image' alt='BullsEye Axe Lounge'/>
 
         <NavLink
-          class='link item'
+          className='link item'
           to='/'
           exact
         >Home</NavLink>
 
         <NavLink
-          class='link item'
+          className='link item'
           to='/'
           exact
           >About Us</NavLink>
 
         <NavLink
-          class='link item'
+          className='link item'
           to='/'
           exact
           >FAQ</NavLink>
 
         <NavLink
-          class='link item'
+          className='link item'
           to='/'
           exact
           >Blog</NavLink>
 
         <NavLink
-          class='link item'
+          className='link item'
           to='/'
           exact
           >Gift Cards</NavLink>
 
         <NavLink
-          class='link item'
+          className='link item'
           to='/'
           exact
           >Contact Us</NavLink>
