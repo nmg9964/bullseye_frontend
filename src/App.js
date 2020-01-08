c6bb2f763e9ac987d344f9306e8cf471c40bd77b
NaveedG <nganjani@protonmail.com>
2020-01-08 10:59:58 -0600

    created navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './Navbar.js'
 import 'semantic-ui-css/semantic.min.css'
-import './App.css'
 
 function App() {
   return (
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,48 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 class Navbar extends React.Component {
+  
   render() {
     return (
-      <div>
-          <img src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' class="ui big top aligned image"/>
+      <div class='ui secondary menu'>
+        <img src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' class='ui big image'/>
+
+        <NavLink
+          class='link item'
+          to='/'
+          exact
+        >Home</NavLink>
+
+        <NavLink
+          class='link item'
+          to='/about'
+          exact
+          >About Us</NavLink>
+
+        <NavLink
+          class='link item'
+          to='/faq'
+          exact
+          >FAQ</NavLink>
+
+        <NavLink
+          class='link item'
+          to='/blog'
+          exact
+          >Blog</NavLink>
+
+        <NavLink
+          class='link item'
+          to='/gift-cards'
+          exact
+          >Gift Cards</NavLink>
+
+        <NavLink
+          class='link item'
+          to='/contact-us'
+          exact
+          >Contact Us</NavLink>
+    
       </div>
     )
   }
