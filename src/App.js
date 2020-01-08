ab6139770c0f202ce53e38ff389bcb41583fa336
NaveedG <nganjani@protonmail.com>
2020-01-08 10:22:37 -0600

    added files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './Navbar.js'
 import 'semantic-ui-css/semantic.min.css'
+import './App.css'
 
 function App() {
   return (
   <BrowserRouter>
     <div>
+      <Navbar></Navbar>
     </div>
   </BrowserRouter>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+          <img src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' class="ui big top aligned image"/>
+      </div>
+    )
+  }
+}
+
+export default Navbar
