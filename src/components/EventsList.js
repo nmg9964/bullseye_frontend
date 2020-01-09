e16e1ddcbebe92fb847ab1ca9f9d1dfa39d39591
NaveedG <nganjani@protonmail.com>
2020-01-09 14:47:07 -0600

    upcoming and past events listed

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Header } from 'semantic-ui-react'
 import { Button } from 'semantic-ui-react'
 import { withRouter } from 'react-router'
+import EventCard from './EventCard'
 
 class EventsList extends React.Component {
   render () {
+    const today = new Date()
+    const upcomingEvents = this.props.events.filter(event => new Date(event.date) > today)
+    const pastEvents = this.props.events.filter(event => new Date(event.date) < today)
+
     return(
       <div className='App'>
         <Header as='h1'>
           Welcome, {this.props.currentAdmin.username}!
-        </Header>
-        <h3>Upcoming Events</h3>
+        </Header><br></br>
+
+        <h2>Upcoming Events</h2>
         <ul>
-          {this.props.events.map(event => {
+          {upcomingEvents.map(event => {
             return <li>{event.first_name}&nbsp;{event.last_name}</li>
           })}
-        </ul>
+        </ul><br></br>
+
+        <h2>Past Events</h2>
+        <ul>
+          {pastEvents.map(event => {
+            return <li>{event.first_name}&nbsp;{event.last_name}</li>
+          })}
+        </ul><br></br>
+
         <Button primary onClick={this.props.handleOnClick}>
           Logout
         </Button>
