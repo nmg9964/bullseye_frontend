1e97c35fda0e4c662b08f3e2f3822a5f74ddf4d0
NaveedG <nganjani@protonmail.com>
2020-01-10 11:50:47 -0600

    dates and times compared

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -17,8 +17,9 @@ class EventsList extends React.Component {
 
   render () {
     const today = new Date()
-    const upcomingEvents = this.props.events.filter(event => new Date(event.date) > today)
-    const pastEvents = this.props.events.filter(event => new Date(event.date) < today)
+    const currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+    const upcomingEvents = this.props.events.filter(event => new Date(event.date) > today && event.time > currentTime)
+    const pastEvents = this.props.events.filter(event => new Date(event.date) < today || event.time < currentTime)
 
     return(
       <div>
