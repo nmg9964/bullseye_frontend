cf39a93f2550c08f18d877ef34ae05d2c43a6323
NaveedG <nganjani@protonmail.com>
2020-01-14 11:36:40 -0600

    create event form modified

--- src/components/CreateEventForm.js
+++ src/components/CreateEventForm.js
@@ -10,7 +10,7 @@ import "react-datepicker/dist/react-datepicker.css"
 class CreateEventForm extends React.Component {
   state = {
     date: new Date(),
-    time: new Date(),
+    time: '',
     firstName: '',
     lastName: '',
     guestCount: 0,
@@ -44,6 +44,33 @@ class CreateEventForm extends React.Component {
   }
 
   render() {
+    const hour = this.state.time
+    const weekdayOptions = [ 
+      { key: 0, text: '12:00 PM', value: hour },
+      { key: 1, text: '1:00 PM', value: hour },
+      { key: 2, text: '2:00 PM', value: hour },
+      { key: 3, text: '3:00 PM', value: hour },
+      { key: 4, text: '4:00 PM', value: hour },
+      { key: 5, text: '5:00 PM', value: hour },
+      { key: 6, text: '6:00 PM', value: hour },
+      { key: 7, text: '7:00 PM', value: hour },
+      { key: 8, text: '8:00 PM', value: hour }
+    ]
+
+    const weekendOptions = weekdayOptions.concat(
+      { key: 9, text: '9:00 PM', value: hour },
+      { key: 10, text: '10:00 PM', value: hour },
+      { key: 11, text: '11:00 PM', value: hour },
+      { key: 12, text: '12:00 AM', value: hour }
+    )
+
+    const options = () => {
+      if (this.state.date.getDay() === 5 || this.state.date.getDay() === 6)
+        return weekendOptions
+      else
+        return weekdayOptions
+    }
+
     return(
       <div>
         {this.props.renderConfirmPage ?
@@ -51,14 +78,14 @@ class CreateEventForm extends React.Component {
 
         <div className='App'>
           <h1>Book a Session!</h1>
-          <h3>Select a Date/Time</h3>
+          <h3>Select a Date</h3>
 
           <DatePicker
           selected={this.state.date}
           onChange={this.handleDateChange}
           value={this.state.date}/>
 
-          <DatePicker 
+          {/* <DatePicker 
           selected={this.state.time} 
           onChange={this.handleTimeChange}
           value={this.state.time}
@@ -66,10 +93,15 @@ class CreateEventForm extends React.Component {
           showTimeSelectOnly
           timeIntervals={60}
           timeCaption="Time"
-          dateFormat="hh:mm aa"/>
+          dateFormat="hh:mm aa"/> */}
 
           <Form onSubmit={this.handleOnSubmit}>
           <Form.Group widths='equal'/>
+            <Form.Select 
+            fluid label='Available Times*'
+            options={options()}
+            onChange={this.handleOnChange}
+            placeholder='12:00 PM' />
             <Form.Input fluid label='First name*' type='text' name='firstName' value={this.state.firstName} placeholder='First name' onChange={this.handleOnChange} />
             <Form.Input fluid label='Last name*' type='text' name='lastName' value={this.state.lastName} placeholder='Last name' onChange={this.handleOnChange}/>
             <Form.Input fluid label='Number of guests*' type='number' max='10' name='guestCount' value={this.state.guestCount} placeholder='Number of guests' onChange={this.handleOnChange}/>
