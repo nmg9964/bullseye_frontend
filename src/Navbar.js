ddb70f76dac222fc1d1e5eb4516c0f09acbca31d
NaveedG <nganjani@protonmail.com>
2020-01-20 21:17:12 -0600

    css styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,47 +1,53 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react'
+import { Menu, Image } from 'semantic-ui-react'
 
 class Navbar extends React.Component {
   
   render() {
     return (
       <div className='navbar'>
-      <Menu secondary>
-        <img src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' className='ui big image' alt='BullsEye Axe Lounge'/>
-
+      <Menu secondary size='large'>
+          <Image size='mini' src='https://www.bullseyeaxelounge.com/wp-content/themes/Website55889577/images/logobig.png' className='ui big image' alt='BullsEye Axe Lounge'/>
+        
         <NavLink
           className='link item'
+          activeClassName='menu'
           to='/'
           exact
         >Home</NavLink>
 
         <NavLink
           className='link item'
+          activeClassName='menu'
           to='/'
           exact
           >About Us</NavLink>
 
         <NavLink
           className='link item'
+          activeClassName='menu'
           to='/'
           exact
           >FAQ</NavLink>
 
         <NavLink
           className='link item'
+          activeClassName='menu'
           to='/'
           exact
           >Blog</NavLink>
 
         <NavLink
           className='link item'
+          activeClassName='menu'
           to='/'
           exact
           >Gift Cards</NavLink>
 
         <NavLink
           className='link item'
+          activeClassName='menu'
           to='/'
           exact
           >Contact Us</NavLink>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { Grid, Button, Embed, Image } from 'semantic-ui-react'
+import { Grid, Button, Embed, Image, Header } from 'semantic-ui-react'
 
 class Home extends React.Component {
 
     render () {
       return(
-        <Grid centered columns={2} className='App-header'>
+        <div className='App-header'>
+        <Header size='huge' color='grey' textAlign='center'>
+          AXE THROWING IN THE ARBORETUM OF SOUTH BARRINGTON
+        </Header>
+
+        <Header size='large' color='grey' textAlign='center'>
+          COMING SOON TO THE STREETS OF WOODFIELD
+        </Header>
+
+        <Grid centered columns={2}>
+
           <Grid.Column>
-            <Embed id='T_apDtxNZYc' source='youtube' autoplay={true}/>
+            <Embed
+            id='lTno4Aw3O_U'
+            source='youtube'
+            autoplay={true}
+            active={true} />
           </Grid.Column>
+
           <Grid.Row>
-          <Button secondary onClick={() => this.props.history.push('/book-session')}>Book Now!</Button>
+          <Button  size='massive' secondary onClick={() => this.props.history.push('/book-session')}>Book Now!</Button>
           </Grid.Row>
+
         </Grid>
+        </div>
       )
     }
   }
