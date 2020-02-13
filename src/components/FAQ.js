c7feb58e7b9635b2c3c3e2ed356e26e6f3d883c5
NaveedG <nganjani@protonmail.com>
2020-02-12 21:54:09 -0600

    faq updated

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -25,6 +25,22 @@ class FAQ extends React.Component {
             <p>*Note that couples may need to share a booth with another pair, only when we are at full capacity. In this case, you will still always have your own private range and target.</p>
           </ul><br></br>
 
+          <p style={{fontWeight: 'bold'}}>Are you on social media?</p>
+          <p>Yes we do! Please visit our Instagram, Facebook and LinkedIn.</p>
+
+          <p style={{fontWeight: 'bold'}}>How do I set a up a birthday party, corporate, or special event?</p>
+          <p>Book here or contact us at 630-550-8939 or ben@bullseyeaxelounge.com</p>
+
+          <p style={{fontWeight: 'bold'}}>How many people can I bring?</p>
+          <p>As many as you want! (up to 60 people if the whole facility is needed)
+            The more the merrier! But, to make sure no one is turned away, we strongly encourage you to make an advance reservation here.</p>
+
+          <p style={{fontWeight: 'bold'}}>Do I need to make a reservation or can I walk in at any time?</p>
+          <p>We can typically accommodate most walk-ins, outside of our peak hours after 5pm on Fridays and Saturdays. Making your reservation in advance takes only a few seconds, and protects everyone from any disappointments. To book, please click here.</p>
+
+          <p style={{fontWeight: 'bold'}}>How old do you have to be to throw axes?</p>
+          <p>We do not have a specific age limit, but a coach or manager must determine that the willing, young thrower can safely hold, and throw the axe to the target from 10ft.</p>
+
         </div>  
 
       </div>
