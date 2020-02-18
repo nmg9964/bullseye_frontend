1cae47d86e1f600984d0ded4ac12256b31ea4096
NaveedG <nganjani@protonmail.com>
2020-02-18 15:52:38 -0600

    links added to faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -11,10 +11,10 @@ class FAQ extends React.Component {
 
         <div className='text-area'>
           <p style={{fontWeight: 'bold'}}>What are your prices?</p>
-          <p>Take advantage of our weekend group pricing, special weekday prices, and more – view here!</p><br></br>
+          <p>Take advantage of our weekend group pricing, special weekday prices, and more – view <a href='http://localhost:3000/about-us'>here!</a></p><br></br>
 
           <p style={{fontWeight: 'bold'}}>When are you open?</p>
-          <p>See our standard business hours here. If you would like to book please click here. If you have a large party, special event or private facility, you may call 630-550-8939 or email ben@bullseyeaxelounge.com.</p>
+          <p>See our standard business hours <a href='http://localhost:3000/'>here</a>. If you would like to book a session, please click <a href='http://localhost:3000/book-session'>here</a>. If you have a large party, special event or private facility, you may call 630-550-8939 or email ben@bullseyeaxelounge.com.</p>
 
           <p style={{fontWeight: 'bold'}}>What do I get in an axe throwing session?</p>
           <ul>
@@ -26,17 +26,17 @@ class FAQ extends React.Component {
           </ul><br></br>
 
           <p style={{fontWeight: 'bold'}}>Are you on social media?</p>
-          <p>Yes we do! Please visit our Instagram, Facebook and LinkedIn.</p>
+          <p>Yes we do! Please visit our <a href='https://www.instagram.com/axelounge/'>Instagram</a>, <a href='https://www.facebook.com/bullseyeaxelounge/'>Facebook</a> and <a href='https://www.linkedin.com/company/bullseye-axe-lounge'>LinkedIn</a>.</p>
 
           <p style={{fontWeight: 'bold'}}>How do I set a up a birthday party, corporate, or special event?</p>
-          <p>Book here or contact us at 630-550-8939 or ben@bullseyeaxelounge.com</p>
+          <p>Book <a href='http://localhost:3000/book-session'>here</a> or contact us at 630-550-8939 or ben@bullseyeaxelounge.com</p>
 
           <p style={{fontWeight: 'bold'}}>How many people can I bring?</p>
           <p>As many as you want! (up to 60 people if the whole facility is needed)
-            The more the merrier! But, to make sure no one is turned away, we strongly encourage you to make an advance reservation here.</p>
+            The more the merrier! But, to make sure no one is turned away, we strongly encourage you to make an advance reservation.</p>
 
           <p style={{fontWeight: 'bold'}}>Do I need to make a reservation or can I walk in at any time?</p>
-          <p>We can typically accommodate most walk-ins, outside of our peak hours after 5pm on Fridays and Saturdays. Making your reservation in advance takes only a few seconds, and protects everyone from any disappointments. To book, please click here.</p>
+          <p>We can typically accommodate most walk-ins, outside of our peak hours after 5pm on Fridays and Saturdays. Making your reservation in advance takes only a few seconds, and protects everyone from any disappointments. To book, please click <a href='http://localhost:3000/book-session'>here</a>.</p>
 
           <p style={{fontWeight: 'bold'}}>How old do you have to be to throw axes?</p>
           <p>We do not have a specific age limit, but a coach or manager must determine that the willing, young thrower can safely hold, and throw the axe to the target from 10ft.</p>
