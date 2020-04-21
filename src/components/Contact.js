0d286ba2df3db9d6347cf34f516aba33a415343f
NaveedG <nganjani@protonmail.com>
2020-04-21 18:02:02 -0500

    contact us component updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,17 +5,20 @@ class Contact extends React.Component {
   render() {
     return <div className='App-home'>
       <h1 className='label'>Contact Us</h1>
-      <div className='text-area'>
-        <p>To book an axe-throwing session, please <a href='http://localhost:3000/book-session'>click here</a>.
-        <br></br>Phone: 630-550-8939
-        <br></br>Email: info@bullseyeaxelounge.com</p>
-      </div>
 
       <Divider section hidden />
 
       <Grid container columns={2}>
 
         <Grid.Column>
+          <h1 className='fields'>LOCATED IN THE ARBORETUM OF SOUTH BARRINGTON</h1>
+          <div className='text-area'>
+            <p>100 W Higgins Rd #M30 
+            <br></br>South Barrington, IL 60010</p>
+            <p>To book an axe-throwing session, please <a href='http://localhost:3000/book-session'>click here</a>.
+            <br></br>Phone: 630-550-8939
+            <br></br>Email: info@bullseyeaxelounge.com</p>
+          </div>
         </Grid.Column>
       
       </Grid>
